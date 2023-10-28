7cebd5cfda59126ad7da0c4e72a98928d1b1a1ed
Joan <joan300501@gmail.com>
2023-10-28 16:28:58 -0700

    Font Variables with tailwindcss, Image Responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
+import "./ui/global.css";
+import { inter, lusitana } from "./lib/fonts";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${inter.variable} ${lusitana.variable} `}>
+      <body className="font-inter">{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+import AcmeLogo from "@/app/ui/acme-logo";
 
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+          <p className="font-lusitana text-xl text-gray-800 md:text-3xl md:leading-normal">
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
@@ -24,7 +25,22 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            priority
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            priority
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
         </div>
       </div>
     </main>
