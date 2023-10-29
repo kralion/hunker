8dfbef0c3255901c39ab17e2323544ba139c37f3
Joan <joan300501@gmail.com>
2023-10-29 10:01:06 -0500

    Resolved #2 Data Feching (Parallel, Waterfall)

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,30 @@
+import Cards from "@/app/ui/dashboard/cards";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import {
+  CardsSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "@/app/ui/skeletons";
+import { Suspense } from "react";
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className="font-lusitana mb-4 text-xl md:text-2xl">Dashboard</h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <Cards />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,18 @@
 import "./ui/global.css";
 import { inter, lusitana } from "./lib/fonts";
+import { Metadata } from "next";
+
+export const meta: Metadata = {
+  title: "Acme Inc. - Next.js 14 Starter",
+  description: "A starter for Next.js 14 through Nextjs/Learn",
+  icons: [
+    {
+      url: "/favicon.png",
+      type: "image/x-icon",
+    },
+  ],
+};
+
 export default function RootLayout({
   children,
 }: {
